e94094f43f09db9466145607d5b17f1d63b084c3
Zane Phillips <zane.phillips@3db-labs.com>
2024-05-04 09:09:53 +0100

    Finish interfacing with hardware

--- PlantPi.py
+++ PlantPi.py
@@ -2,45 +2,48 @@
 from gpiozero import AnalogInputDevice,DigitalOutputDevice
 from time import sleep
 import board
-import busio
-import Adafruit-ADS1x15.ads1115 as ADS
-from Adafruit-ADS1x15.analog_in import AnalogIn
+import Adafruit_ADS1x15 as ADS
 
-class ADS1115(AnalogInputDevice):
+# class ADS1115(AnalogInputDevice):
     
-    def __init__(self, channel, differential=False, **spi_args):
-        self.channel = channel
+#     def __init__(self, channel, differential=False, **spi_args):
+#         self.channel = channel
         
-        self.differential = differential
-        super().__init__(bits=16, max_voltage=3.0, **spi_args)	
+#         self.differential = differential
+#         super().__init__(bits=16, max_voltage=3.0, **spi_args)	
         
-    @property
-    def channel(self):
-        return self.channel
+#     @property
+#     def channel(self):
+#         return self.channel
     
-    @property
-    def differential(self):
-        return self.differential
+#     @property
+#     def differential(self):
+#         return self.differential
     
-    def _read(self):
-        return self._words_to_int(self._spi.transfer(self._send())[-2:], self.bits)
+#     def _read(self):
+#         return self._words_to_int(self._spi.transfer(self._send())[-2:], self.bits)
 
-pump = DigitalOutputDevice(14, active_high=True)
+pump = DigitalOutputDevice(14, active_high=False)
 
 # Create the I2C bus
-i2c = busio.I2C(board.SCL, board.SDA)
+# i2c = ADS.I2C(board.SCL, board.SDA)
 
 # Create the ADC object using the I2C bus
-ads = ADS.ADS1115(i2c)
+ads = ADS.ADS1115()
 
 # Create single-ended input on channel 0
-chan1 = AnalogIn(ads, ADS.P0)
-chan2 = AnalogIn(ads, ADS.P1)
-chan3 = AnalogIn(ads, ADS.P2)
-chan4 = AnalogIn(ads, ADS.P3)
+# chan1 = AnalogIn(ads, ADS.P0)
+# chan2 = AnalogIn(ads, ADS.P1)
+# chan3 = AnalogIn(ads, ADS.P2)
+# chan4 = AnalogIn(ads, ADS.P3)
 
 while True:
-    print("{:>5}\t{:>5.3f} | {:>5}\t{:>5.3f} | {:>5}\t{:>5.3f} | {:>5}\t{:>5.3f}".format(chan1.value, chan1.voltage, chan2.value, chan2.voltage, chan3.value, chan3.voltage, chan4.value, chan4.voltage))
+    chans = []
+    for i in range(4):
+        chans.append(ads.read_adc(i))
+    
+    print(chans)
+        
     sleep(1)
 
 # 	print("ON")
