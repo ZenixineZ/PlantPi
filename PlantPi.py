b98c3503e552cf7b19c54d88530059e83d41df65
Zane Phillips <zane.phillips@3db-labs.com>
2024-06-07 06:09:34 -0400

    Refactor into a server/client model

--- PlantPi.py
+++ PlantPi.py
@@ -4,11 +4,8 @@ import time
 from datetime import datetime
 from time import sleep
 import Adafruit_ADS1x15 as ADS
-import os
-import matplotlib.pyplot as plt
 import sys
-
-plt.switch_backend('Qt5Agg')
+import requests
 
 ## TODO:
 #   -DEV:
@@ -20,7 +17,6 @@ plt.switch_backend('Qt5Agg')
 #       -figure out threshold mapping to standard scales for light and mositure
 #       -test moisture handling and revise thresholding if needed
 #       - 
-
 
 
 #   Moisture Mapping, tested with resistive gardening probe
@@ -52,7 +48,7 @@ class ChannelSpec:
         
 class PlantProfile:
     def __init__(self, name, moisture_min, moisture_max, light_min, light_max):
-        assert moisture_min > 0 and moisture_max > 0 and light_min > 0 and light_max > 0
+        assert moisture_min >= 0 and moisture_max >= 0 and light_min >= 0 and light_max >= 0
         self.name = name
         self.moisture_min = moisture_min/10
         self.moisture_max = moisture_max/10
@@ -61,21 +57,18 @@ class PlantProfile:
 
 class PlantPi:
     def __init__(self, plant_profile : PlantProfile, relay_gpio=14, channel_spec=ChannelSpec(), fill_time=5, fill_pad=0.9):
-        self.data_file = None
         self.plant_profile = plant_profile
         assert relay_gpio < 26
         self.pump = DigitalOutputDevice(relay_gpio, active_high=False)
         self.channel_spec = channel_spec
 
-        self.times = []
-        self.t0 = None
-        self.moisture_top_buff = []
-        self.moisture_bottom_buff = []
-        self.light1_buff = []
-        self.light2_buff = []
+        self.time = 0
+        self.moisture_top = 0
+        self.moisture_bottom = 0
+        self.light1 = 0
+        self.light2 = 0
         # Create the ADC object using the I2C bus
         self.adc = ADS.ADS1115()
-        self.cnt = 0
         self.need_fill = False
         self.need_top_off = False
         self.start_fill = None
@@ -84,50 +77,33 @@ class PlantPi:
         assert fill_time > 0
         self.fill_pad = fill_pad
         assert fill_pad > 0
-        write_header = not os.path.isfile("data.csv")
-        self.data_file = open("data.csv", "a")
-        if write_header:
-            self.data_file.write("TIME,TOP MOISTURE,BOTTOM MOISTURE,LIGHT 1,LIGHT 2,PUMP STATE,PLANT PROFILE\n")
+        d = { \
+                'name': plant_profile.name, \
+                'moisture_min': plant_profile.moisture_min, \
+                'moisture_max': plant_profile.moisture_maxv, \
+                'light_min': plant_profile.light_min, \
+                'light_max': plant_profile.light_max \
+            }
+        requests.post('http://127.0.0.1:8080/plant', json=d)
+
         
-    def __del__(self):
-        if(self.data_file):
-            self.data_file.close()
-
-    def log(self, string):
-        print(get_time() + ": " + string)
-
-    def check_light_out_of_range(self):
-        light1 = self.light1_buff[-1]
-        light2 = self.light2_buff[-1]
-        if light1 > self.plant_profile.light_max:
-            self.log("Sensor 1 Too Bright")
-        if light1 < self.plant_profile.light_min:
-            self.log("Sensor 1 Too Dim")
-        if light2 > self.plant_profile.light_max:
-            self.log("Sensor 2 Too Bright")
-        if light2 < self.plant_profile.light_min:
-            self.log("Sensor 2 Too Dim")
             
     def water(self):
         if(self.pump.value == 0):
             self.pump.on()
-            self.log("Started Watering")
     
     def stop_watering(self):
         if(self.pump.value == 1):
             self.pump.off()
-            self.log("Stopped Watering")
 
     def water_if_thirsty(self):
-        moisture_top = self.moisture_top_buff[-1]
-        moisture_bottom = self.moisture_bottom_buff[-1]
         # If we don't need to fill, check against the low threshold, otherwise, check against the high threshold so we fill it up to that point
         thresh = self.plant_profile.moisture_min
         if self.need_fill:
             thresh = self.plant_profile.moisture_max
 
         # If both sensors are above the threshold, dont water
-        if moisture_bottom > thresh and moisture_top > thresh:
+        if self.moisture_bottom > thresh and self.moisture_top > thresh:
             self.need_fill = False
             self.need_top_off = False
             self.pause_fill = None
@@ -136,17 +112,17 @@ class PlantPi:
         # If the bottom sensor is below the threshold, water for fill_time seconds, 
         # then wait for 2*fill_time seconds and then repeat if needed to let the water settle.
         # When filling, stop at 100*fill_coef % of max moisture level (for bottom sensor) to let water settle
-        if (not self.need_fill and moisture_bottom < thresh) or (self.need_fill and moisture_bottom < self.fill_pad*thresh):
+        if (not self.need_fill and self.moisture_bottom < thresh) or (self.need_fill and self.moisture_bottom < self.fill_pad*thresh):
             self.need_fill = True
             self.need_top_off = False                
 
-            if (not self.pause_fill) and self.start_fill and self.times[-1] - self.start_fill > self.fill_time:
-                self.pause_fill = self.times[-1]
-            if self.pause_fill and self.times[-1] - self.pause_fill < 2*self.fill_time:
+            if (not self.pause_fill) and self.start_fill and self.time - self.start_fill > self.fill_time:
+                self.pause_fill = self.time
+            if self.pause_fill and self.time - self.pause_fill < 2*self.fill_time:
                 self.start_fill = None
                 return self.stop_watering()
             if not self.start_fill:
-                self.start_fill = self.times[-1]
+                self.start_fill = self.time
             self.pause_fill = None
             return self.water()
         
@@ -154,88 +130,46 @@ class PlantPi:
         self.pause_fill = None
         self.start_fill = None
         # Otherwise, only the top sensor below the threshold, water until it isn't
-        if moisture_top < thresh:
+        if self.moisture_top < thresh:
             self.need_top_off = True
             return self.water()
-        self.cnt = 0
         self.need_top_off = False
-        return self.stop_watering()
-
-    def read_data(self):
-            if not self.t0:
-                self.t0 = time.time()
-            t = time.time()
-            moisture_top = map_moisture(self.adc.read_adc(self.channel_spec.moisture_top)/32767)
-            moisture_bottom = map_moisture(self.adc.read_adc(self.channel_spec.moisture_bottom)/32767)
-            light1 = self.adc.read_adc(self.channel_spec.light1)/32767
-            light2 = self.adc.read_adc(self.channel_spec.light2)/32767
-            self.times.append(t)
-            self.moisture_top_buff.append(moisture_top)
-            self.moisture_bottom_buff.append(moisture_bottom)
-            self.light1_buff.append(light1)
-            self.light2_buff.append(light2)
-            self.data_file.write(','.join([str(t), str(moisture_top), str(moisture_bottom), str(light1), str(light2), str(self.pump.value == 1), self.plant_profile.name])+'\n')
-            while self.times[-1]-self.times[0] > 60*60*12:
-                del self.times[0]
-                del self.moisture_top_buff[0]
-                del self.moisture_bottom_buff[0]
-                del self.light1_buff[0]
-                del self.light2_buff[0]
+        return self.stop_watering()           
                 
-    def graph(self):
-        if len(self.times) == 1:
-            plt.ion()
-            self.fig, self.sps = plt.subplots(nrows=2,ncols=1)
-            self.sps[0].set_title('Top & Bottom Moisture')
-            self.sps[0].set_ylim(0, 10)
-            self.sps[1].set_title('Light 1 & Light 2')
-            self.sps[1].set_ylim(0, 10)
-            self.sps[0].grid()
-            self.sps[1].grid()
-
-            self.plot_mt, = self.sps[0].plot([],[],'b-')
-            self.plot_mb, = self.sps[0].plot([],[],'r-')
-            self.plot_l1, = self.sps[1].plot([],[],'c-')
-            self.plot_l2, = self.sps[1].plot([],[],'m-')
-            plt.show(block=False)
-        if len(self.times) > 1:
-            t =  [x - self.t0 for x in self.times]
-            mt = [x * 10 for x in self.moisture_top_buff]
-            mb = [x * 10 for x in self.moisture_bottom_buff]
-            l1 = [x * 10 for x in self.light1_buff]
-            l2 = [x * 10 for x in self.light2_buff]
-            self.plot_mt.set_data(t, mt)
-            self.plot_mb.set_data(t, mb)
-            self.plot_l1.set_data(t, l1)
-            self.plot_l2.set_data(t, l2)
-            self.sps[0].legend(loc="upper right", labels=['Top: '+str(mt[-1])[0:4],'Bottom: '+str(mb[-1])[0:4]])                                                                                                                
-            self.sps[1].legend(loc="upper right", labels=['1: '+str(l1[-1])[0:4],'2: '+str(l2[-1])[0:4]])
-            
-            self.sps[0].set_xlim(min(t), max(t))
-            self.sps[1].set_xlim(min(t), max(t))
-            self.fig.canvas.draw()
-            self.fig.canvas.flush_events()
-            plt.show(block=False)
-
     def run(self):
         try:
             while True:
-                self.read_data()
+                self.time = time.time()
+                self.moisture_top = map_moisture(self.adc.read_adc(self.channel_spec.moisture_top)/32767)
+                self.moisture_bottom = map_moisture(self.adc.read_adc(self.channel_spec.moisture_bottom)/32767)
+                self.light1 = self.adc.read_adc(self.channel_spec.light1)/32767
+                self.light2 = self.adc.read_adc(self.channel_spec.light2)/32767
+                
                 self.water_if_thirsty()    
-                self.check_light_out_of_range()
-                self.graph()
-                # Use a shorter 0.25 sec update when watering and a 30 min update otherwise
+                d = { \
+                     'time': self.time, \
+                     'moisture_top': self.moisture_top, \
+                     'moisture_bottom': self.moisture_bottom, \
+                     'light1': self.light1, \
+                     'light2': self.light2, \
+                     'pump': self.pump.value == 1 \
+                    }
+                
+                retry = 0
+                while retry < 5:
+                    try:
+                        requests.post('http://127.0.0.1:8080/data', json=d)
+                    except requests.exceptions.RequestException:
+                        pass                                # Use a shorter 0.25 sec update when watering and a 30 min update otherwise
                 if self.need_top_off or self.need_fill or (len(sys.argv) > 1  and sys.argv[1] == '-t'):
                     sleep(0.25)
                 else:
                     sleep(1800)
         except KeyboardInterrupt:
             self.stop_watering()
-            plt.pause(3)
-            plt.close("all")
 
 if __name__ == "__main__":
-    palm = PlantProfile(name="Majestic Palm", moisture_min=3, moisture_max=7, light_min=4, light_max=6)
-    dracaena = PlantProfile(name="Dragon Plant", moisture_min=3, moisture_max=7, light_min=4, light_max=7)
-    pp = PlantPi(dracaena)
+    palm = PlantProfile(name="Majestic Palm", moisture_min=3, moisture_max=7, light_min=4, light_max=6) # light vals are placeholders
+    dracaena = PlantProfile(name="Dragon Plant", moisture_min=3, moisture_max=7, light_min=4, light_max=7) # light vals are placeholders
+    pp = PlantPi(PlantProfile(name="TEST", moisture_min=0, moisture_max=0, light_min=0, light_max=10))
     pp.run()
---
+++ PlantPiServer.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 13 17:02:30 2024
+
+@author: zphillips
+"""
+import os
+import matplotlib.pyplot as plt
+plt.switch_backend('Qt5Agg')
+
+from datetime import datetime
+
+from flask import Flask, jsonify, request
+import json
+import time
+
+t0 = None
+data_file = None
+pump = False
+times = []
+moisture_top_buff = []
+moisture_bottom_buff = []
+light1_buff = []
+light2_buff = []
+
+plant_name = 'NONE'
+moisture_max = 0
+moisture_min = 0
+light_max = 10
+light_min = 0
+
+def get_time():
+    return datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S.%f')
+
+def check_light_out_of_range():
+    light1 = light1_buff[-1]
+    light2 = light2_buff[-1]
+    if light1 > light_max:
+        log("Sensor 1 Too Bright")
+    if light1 < light_min:
+        log("Sensor 1 Too Dim")
+    if light2 > light_max:
+        log("Sensor 2 Too Bright")
+    if light2 < light_min:
+        log("Sensor 2 Too Dim")
+        
+def log(string):
+    print(get_time() + ": " + string)
+        
+def graph():
+    if len(times) == 1:
+        t0 = times[0]
+        plt.ion()
+        fig, sps = plt.subplots(nrows=2,ncols=1)
+        sps[0].set_title('Top & Bottom Moisture')
+        sps[0].set_ylim(0, 10)
+        sps[1].set_title('Light 1 & Light 2')
+        sps[1].set_ylim(0, 10)
+        sps[0].grid()
+        sps[1].grid()
+
+        plot_mt, = sps[0].plot([],[],'b-')
+        plot_mb, = sps[0].plot([],[],'r-')
+        plot_l1, = sps[1].plot([],[],'c-')
+        plot_l2, = sps[1].plot([],[],'m-')
+        plt.show(block=False)
+    if len(times) > 1:
+        t =  [x - t0 for x in times]
+        mt = [x * 10 for x in moisture_top_buff]
+        mb = [x * 10 for x in moisture_bottom_buff]
+        l1 = [x * 10 for x in light1_buff]
+        l2 = [x * 10 for x in light2_buff]
+        plot_mt.set_data(t, mt)
+        plot_mb.set_data(t, mb)
+        plot_l1.set_data(t, l1)
+        plot_l2.set_data(t, l2)
+        sps[0].legend(loc="upper right", labels=['Top: '+str(mt[-1])[0:4],'Bottom: '+str(mb[-1])[0:4]])                                                                                                                
+        sps[1].legend(loc="upper right", labels=['1: '+str(l1[-1])[0:4],'2: '+str(l2[-1])[0:4]])
+        
+        sps[0].set_xlim(min(t), max(t))
+        sps[1].set_xlim(min(t), max(t))
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+        plt.show(block=False)
+
+app = Flask(__name__)
+@app.route("/data", methods=['POST'])
+def data():
+    d = json.loads(request.data)
+    times.append(d['time'])
+    moisture_top_buff.append(d['moisture_top'])
+    moisture_bottom_buff.append(d['moisture_bottom'])
+    light1_buff.append(d['light1'])
+    light2_buff.append(d['light2'])
+    if pump != d['pump']:
+        if d['pump']:
+            log("Started Watering")
+        else:
+            log("Stopped Watering")
+        pump = d['pump']
+    data_file.write(','.join([str(times[-1]), str(moisture_top_buff[-1]), str(moisture_bottom_buff[-1]), str(light2_buff[-1]), str(light1_buff[-1]), str(pump), plant_name])+'\n')
+    while times[-1]-times[0] > 60*60*12:
+        del times[0]
+        del moisture_top_buff[0]
+        del moisture_bottom_buff[0]
+        del light1_buff[0]
+        del light2_buff[0]
+                
+@app.route("/plant", methods=['POST'])
+def limits():
+    l = json.loads(request.data)
+    plant_name = l['name']
+    moisture_max = l['moisture_max']
+    moisture_min = l['moisture_min']
+    light_max = l['light_max']
+    light_min = l['light_min']
+
+if __name__ == '__main__':
+    write_header = not os.path.isfile("data.csv")
+    data_file = open("data.csv", "a")
+    if write_header:
+        data_file.write("TIME,TOP MOISTURE,BOTTOM MOISTURE,LIGHT 1,LIGHT 2,PUMP STATE,PLANT PROFILE\n")
+    try:
+        app.run(host='127.0.0.1', port=8080, )
+    except KeyboardInterrupt:
+        data_file.close()
+        plt.close("all")
